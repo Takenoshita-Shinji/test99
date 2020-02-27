781ee9d3f23eb20c1b81e9024ec92fbaddc75e3c
tshinji0616 <tshinji@starsystems.co.jp>
2020-02-28 07:03:05 +0900

    すすっす

--- src/test001/Test.java
+++ src/test001/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test001Ç≈Ç∑ÅB");
+		System.out.println("Test001Ç≈Ç∑ÅBÇ∑Ç∑Ç∑Ç∑");
 
 	}
 
